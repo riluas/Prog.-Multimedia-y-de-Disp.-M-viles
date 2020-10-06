7b1ad010cc56a4d446b206ff9965b968d7a8aa3b
riluas <cutsay48@gmail.com>
2020-10-06 16:23:54 +0200

    AE1-2 Terminada

---
+++ AE1/ae1_2.js
@@ -0,0 +1,9 @@
+let meses =["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+
+//A.
+for (let i = 0; i < meses.length; i++) {
+    console.log(meses[i]);   
+}
+
+//B.
+console.table(meses); 
