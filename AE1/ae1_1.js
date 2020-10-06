e349e5de0459ced8d37731c1fae1b52a81edbcc9
riluas <cutsay48@gmail.com>
2020-10-06 16:15:53 +0200

    AE1-1 Terminada

--- AE1/ae1_1.js
+++ AE1/ae1_1.js
@@ -1,1 +1,12 @@
 console.log("Hola Mundo!");
+
+//A.
+alert("Soy el primer script");
+
+//B.
+let alertMessage = "Soy el Segundo script";
+alert(alertMessage);
+
+//C.
+const ALERT_MESSAGE =  "y estoy funcionando sobre "+ navigator.appVersion;
+alert(ALERT_MESSAGE);
