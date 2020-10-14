5f7295ae226612e76f1eab2e6be93edc32667490
riluas <cutsay48@gmail.com>
2020-10-14 23:17:36 +0200

    AE2-3 Arreglada

--- AE2/ae2_3.js
+++ AE2/ae2_3.js
@@ -2,7 +2,8 @@
 let values = [6,4,6,8,6,4,10,50,220,23];
 
 function findMinimum(v) {
-  console.log(v.reduce((a,b) => Math.min(a, b)));
+  let a = v.reduce((a,b) => Math.min(a, b))
+  return a;  
 };
-
+console.log(findMinimum(values));
 findMinimum(values);
