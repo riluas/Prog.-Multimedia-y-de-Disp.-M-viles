41ecefd4ed160d715602e5a06f7d05d99b0331bf
riluas <cutsay48@gmail.com>
2020-10-14 23:15:04 +0200

    AE2-4 Arreglada

--- AE2/ae2_4.js
+++ AE2/ae2_4.js
@@ -2,7 +2,8 @@
 let values = [45,78,2,4,56,4,48,1,23,4,8,7];
 
 function findGreaterThan(x, v) {
-    console.log(v.some(a  => a > x , v[0]));
+    let a = v.some(a  => a > x , v[0])
+    return a;
 }
-
+console.log(findGreaterThan(10,values));
 findGreaterThan(10,values);
