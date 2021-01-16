596ddfef5c5a4070228966da6019e56530a134d1
riluas <cutsay48@gmail.com>
2021-01-16 23:54:54 +0100

    users

--- pmb-backoffice/src/App.js
+++ pmb-backoffice/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Navbar from './navbar';
-
+import Usuarios from './paginas/usuarios';
 const App = () => (
     <div className="App">
       <Navbar/>
+      <Usuarios/>
+
 
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- pmb-backoffice/src/paginas/usuarios.js
+++ pmb-backoffice/src/paginas/usuarios.js
@@ -1,23 +1,70 @@
-const promise = new Promise((resolve, reject) =>{
-    const request = new XMLHttpRequest();
-    request.timeout = 5;
-    request.open('GET','https://localhost:44349/api/Usuarios');
+import {Component} from 'react';
+const users = [];
+class Usuarios extends Component{
 
-    request.onload = () =>{
-        if (request.status === 200) {
-            resolve(request.response);
-        }else{
-            reject('Algo ha ido mal');
-        }
+    render(){
+
+
+        const promise = new Promise((resolve, reject) =>{
+            const request = new XMLHttpRequest();
+            request.timeout = 5;
+            request.open('GET','https://localhost:44349/api/Usuarios');
+
+            request.onload = () =>{
+                if (request.status === 200) {
+                    resolve(request.response);
+                }else{
+                    reject('Algo ha ido mal');
+                }
+            }
+            request.send();
+        })
+
+        const promiseResult = promise.then((resolvedResult) => {
+            const result = JSON.parse(resolvedResult);
+
+           let contador = 0
+            result.forEach(i => {
+                users.push(result[contador]);
+                contador++
+
+            });
+
+        },(rejectResult)=>{
+         console.error(rejectResult);
+        });
+        console.log(users,"hola");
+        console.log('This is a test after the promise');
+
+
+        return(
+         <div>
+       <table border="1">
+        <thead>
+          <tr>
+            <th>Código</th>
+            <th>Descripción</th>
+            <th>Precio</th>
+          </tr>
+        </thead>
+        <tbody>
+        {console.log(users,"hola2")}
+
+          {/* {this.state.articulos.map(art => {
+            return (
+              <tr key={art.codigo}>
+                <td>{art.codigo}</td>
+                <td>{art.descripcion}</td>
+                <td>{art.precio}</td>
+              </tr>
+            );
+          })} */}
+        </tbody>
+        </table>
+
+          </div>
+        )}
+
     }
-    request.send();
-})
 
-const promiseResult = promise.then((resolvedResult) => {
-    const result = JSON.parse(resolvedResult);
-    console.log(result);
-},(rejectResult)=>{
- console.error(rejectResult);
-});
-
-console.log('This is a test after the promise');
+export default Usuarios;
